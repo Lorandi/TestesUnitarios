b820d5449f0a4ec397d7e903506a9e880700ba8d
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2022-01-17 15:10:45 -0300

    assertNull

--- src/test/java/br/lorandi/service/AssertTest.java
+++ src/test/java/br/lorandi/service/AssertTest.java
@@ -1,13 +1,23 @@
 package br.lorandi.service;
 
+import br.lorandi.entidades.Usuario;
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
+
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class AssertTest {
+    Usuario u1 = new Usuario("Rodrigo");
+    Usuario u2 = new Usuario("Rodrigo");
+    Usuario u3 = u2;
+    Usuario u4 = null;
 
     @Test
     void testTrue(){
@@ -22,5 +32,44 @@ public class AssertTest {
     @Test
     void testEquals(){
         assertEquals(1,1, "Se deu ruim é pq não são iguais");
+    }
+
+    @Test
+    void testDouble(){
+        assertEquals(Math.PI, 3.14, 0.01);
+        // double precisa dos valores e do delta da margem de erro
+    }
+
+    @Test
+    void testInteger(){
+        int i = 5;
+        Integer j = 5;
+        assertEquals(i,j);
+        assertEquals(Integer.valueOf(i),j);
+        assertEquals(i,j.intValue());
+    }
+
+    @Test
+    void testString(){
+        String s1 = "Bola";
+        String s2 = "boLa";
+
+        assertEquals(s1.toUpperCase(Locale.ROOT),s2.toUpperCase(Locale.ROOT));
+        assertTrue(s1.equalsIgnoreCase(s2));
+        assertEquals(true, s1.equalsIgnoreCase(s2));
+    }
+
+    @Test
+    void testUsuario(){
+
+        assertEquals(u1,u2);
+        // verdadeiro quando adicionado o equals na classe Usuario
+
+        assertSame(u3,u2);
+    }
+
+    @Test
+    void testNull(){
+        assertNull(u4);
     }
 }
